0a4bebf32756c38de9eb3c2aa1fb2e129014f1c8
ucupsmith <akilnauffancozz@gmail.com>
2024-02-12 14:29:03 +0700

    updateobejct_user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,19 @@
 import React from "react";
 import Image from "@/Component/Image";
-import {useState} from "react";
-
-
+import { useState, useEffect } from "react";
 
 const itterasi = [
   {
     src: "https://asset.kompas.com/crops/AqnwKTVQ7_mSneRdZQt-slGkiFE=/0x0:1920x1280/750x500/data/photo/2021/12/02/61a8477b01ca9.jpg",
     alt: "",
-    deskripsi: "Hal terakhir yang akan kita lakukan adalah merapikan kode kita sedikit. Seiring bertambahnya ukuran komponen, Anda ingin mengabstraksikan kode sebanyak returnmungkin dari pernyataan. Ini biasanya memerlukan pengaturan output yang Anda inginkan dalam variabel. Caranya cukup mudah",
+    deskripsi:
+      "Hal terakhir yang akan kita lakukan adalah merapikan kode kita sedikit. Seiring bertambahnya ukuran komponen, Anda ingin mengabstraksikan kode sebanyak returnmungkin dari pernyataan. Ini biasanya memerlukan pengaturan output yang Anda inginkan dalam variabel. Caranya cukup mudah",
   },
 
   {
     src: "https://asset.kompas.com/crops/AqnwKTVQ7_mSneRdZQt-slGkiFE=/0x0:1920x1280/750x500/data/photo/2021/12/02/61a8477b01ca9.jpg",
     alt: "",
     deskripsi: "FOTO KUCING 2",
-   
   },
   {
     src: "https://asset.kompas.com/crops/AqnwKTVQ7_mSneRdZQt-slGkiFE=/0x0:1920x1280/750x500/data/photo/2021/12/02/61a8477b01ca9.jpg",
@@ -40,118 +38,118 @@ const itterasi = [
     alt: "",
     deskripsi: "FOTO KUCING 4",
   },
-
-
 ];
 
+const user = {
+  name: "jesica comita",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  desc: "vincent van gogh",
+};
 // const h1 = (() => {
 //   document.querySelector('p container');
-  
+
 // })
 
 // const [btnChg, chCol] = useState('white');
 
-
 const index = () => {
-  
-  const [btnColor, setColor] = useState(false)
-  
-  const eventHandler =()=>{
-    setColor(!btnColor)
-  }
-  
-  
+  const [btnColor, setColor] = useState(false);
+  const [Colorc, setColorCol] = useState(false);
+
+  const eventHandler = () => {
+    setColor(!btnColor);
+  };
+
+  const eventHanz = () => {
+    setColorCol(!Colorc);
+  };
+
   return (
     <>
-
-
-      
       <nav className="bg-white min-h-11 border ">
         <ul className="list-item-group flex justify-between p-5">
           <li>
-            <a href="" className="logo text-black">logo</a>
+            <a href="" className="logo text-black">
+              logo
+            </a>
           </li>
 
           <div className="flex content-end space-x-10">
-
             <li>
-              <a type="button" href="" className="product text-black">product</a>
+              <a type="button" href="" className="product text-black">
+                product
+              </a>
             </li>
             <li>
-              <a href="" className="product text-black">about</a>
+              <a href="" className="product text-black">
+                about
+              </a>
             </li>
             <li>
-              <a href="" className="product text-black">login</a>
+              <a href="" className="product text-black">
+                login
+              </a>
             </li>
-            
           </div>
-          
         </ul>
       </nav>
 
-    
       <div className="container max-w-full">
         <div className="bg-slate-500  min-h-52 my-5 p-10">
           <div className="container-fluid border p-48">
             <h1>card container</h1>
             <p id="p1" className="container">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos saepe veritatis nam minima earum laborum quam, omnis praesentium, minus voluptas tempore sapiente provident autem illo. Nostrum culpa quas consequuntur dolorem.
-              
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos saepe veritatis nam minima earum laborum quam, omnis
+              praesentium, minus voluptas tempore sapiente provident autem illo.
+              Nostrum culpa quas consequuntur dolorem.
             </p>
           </div>
         </div>
       </div>
 
-      
-      
-
       <div className="container justify-between content-center bg-white flex max-w-full p-5 gap-10 my-16 flex-wrap">
-        
-      { itterasi.map((value, index) => {
-        
-        
-        
-        //  const turn:any | number = [clicker,jsClick, 0]
-        const [clicker, jsClick]= useState('grey')
-        
-        return (
-          <div className="align-center p-3 border w-96 ">
+        <div
+          className={`${Colorc ? "bg-orange-400" : "bg-white"}
+      w-96 p-5 space-x-4 border`}
+          onClick={eventHanz}
+        >
+          <img className="logo_react w-96" src={user.imageUrl} />
+          <h1 className="text-black pt-5">{user.desc}</h1>
+        </div>
 
-            <div onClick={eventHandler}
+        {itterasi.map((value, index) => {
+          //  const turn:any | number = [clicker,jsClick, 0]
+          const [clicker, jsClick] = useState("grey");
 
-            className="container-img rounded-lg cursor-pointer">
-                
-                <Image  
-                    
-                    key={index}
-                    src={value.src}
-                    alt={value.alt}
-                    imgclassname="w-96"
-                    
-                  />
-        {/* style={{backgroundColor: `${btnColor}`, transition:'.3s ease', cursor:'pointer'}}  */}
-            </div>  
-      
-                <div className={`${btnColor?"bg-gray-600":"bg-white"} min-h-52 p-5`} >
+          return (
+            <div className="align-center p-3 border w-96 ">
+              <div
+                onClick={eventHandler}
+                className="container-img rounded-lg cursor-pointer"
+              >
+                <Image
+                  key={index}
+                  src={value.src}
+                  alt={value.alt}
+                  imgclassname="w-96"
+                />
+                {/* style={{backgroundColor: `${btnColor}`, transition:'.3s ease', cursor:'pointer'}}  */}
+              </div>
 
-            
-                  <h1 className=" text-black">{value.deskripsi}</h1>
-            
-                </div>
-     
-            
-          </div>
-        );
-      })}
-        
-        
-      </div>    
-      
+              <div
+                className={`${
+                  btnColor ? "bg-gray-600" : "bg-white"
+                } min-h-52 p-5`}
+              >
+                <h1 className=" text-black">{value.deskripsi}</h1>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
-
-
-
 
 export default index;
